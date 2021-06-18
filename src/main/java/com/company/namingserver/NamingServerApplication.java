774f3e8542af8d5679704b4d1da55f02159cabf9
tomra <tom.raeves3we@gmail.com>
2021-06-18 09:34:00 +0200

    added extra error message

--- src/main/java/com/company/namingserver/NamingServerApplication.java
+++ src/main/java/com/company/namingserver/NamingServerApplication.java
@@ -3,6 +3,8 @@ package com.company.namingserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class NamingServerApplication {
 
